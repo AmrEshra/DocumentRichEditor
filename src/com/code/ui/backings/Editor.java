977f0c2c27b761971eff12530fc9f45cd5e9d3fb
Amr Eshra <amr.eshra@gmail.com>
2021-11-03 11:37:54 +0300

    html

--- src/com/code/ui/backings/Editor.java
+++ src/com/code/ui/backings/Editor.java
@@ -1,5 +1,10 @@
 package com.code.ui.backings;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
@@ -14,5 +19,21 @@ public class Editor {
 
 	public void setTextArea(String textArea) {
 		this.textArea = textArea;
+		writeToFile(this.textArea);
+	}
+
+	private void writeToFile(String textArea) {
+		try {
+		
+			FileOutputStream outputStream = new FileOutputStream("D://RichEditor//"+ Math.random() + ".html");
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, "UTF-8");
+            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+             
+            bufferedWriter.write(textArea);
+            bufferedWriter.close();            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		
 	}
 }
